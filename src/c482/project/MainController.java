0c65d0f65a38ac75e379accc99be54284a066319
jbrowe2 <jbrowe2@my.wgu.edu>
2018-02-01 21:40:02 -0700

    #2 - Working on the add screen.

--- src/c482/project/MainController.java
+++ src/c482/project/MainController.java
@@ -7,10 +7,14 @@ package c482.project;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import c482.project.Models.*;
+import java.io.IOException;
+import java.util.logging.*;
+import javafx.event.*;
+import javafx.fxml.*;
+import javafx.scene.*;
+import javafx.stage.*;
 
 /**
  *
@@ -24,6 +28,9 @@ public class MainController implements Initializable {
     @FXML
     private TableView<Product> products;
     
+    @FXML
+    private Button addPart;
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         parts.getItems().addAll(
@@ -34,6 +41,24 @@ public class MainController implements Initializable {
                 new Product(1, "Product 1", 5, 5.00), 
                 new Product(2, "Product 2", 10, 10.58), 
                 new Product(3, "Product 3", 15, 12.3));
+        
+        addPart.setOnAction(new EventHandler<ActionEvent>() {
+            @Override public void handle(ActionEvent e) {
+                try {
+                    FXMLLoader loader = new FXMLLoader(getClass().getResource("AddPart.fxml"));
+                    Parent root = (Parent) loader.load();
+                    
+                    Stage stage = new Stage();
+                    stage.initModality(Modality.APPLICATION_MODAL);
+                    stage.initStyle(StageStyle.UNDECORATED);
+                    stage.setTitle("Add Part");
+                    stage.setScene(new Scene(root)); 
+                    stage.show();
+                } catch (Exception ex) {
+                    Logger.getLogger(MainController.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        });
     }    
     
 }
